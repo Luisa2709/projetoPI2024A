bff0abb71db6e41171bc0f4d4508591a33b03a1e
Luisa2709 <70404564+Luisa2709@users.noreply.github.com>
2024-05-16 17:05:59 -0300

    Update: Pontuação

--- src/main/java/br/maua/t2/_maua_tti101_t2_sistema_academico/telas/PontuacaoProfessor.java
+++ src/main/java/br/maua/t2/_maua_tti101_t2_sistema_academico/telas/PontuacaoProfessor.java
@@ -4,6 +4,10 @@
  */
 package br.maua.t2._maua_tti101_t2_sistema_academico.telas;
 import br.maua.t2._maua_tti101_t2_sistema_academico.db.UsuarioDAO;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JTable;
+
 
 /**
  *
@@ -13,12 +17,16 @@ public class PontuacaoProfessor extends javax.swing.JFrame {
 
     /**
      * Creates new form PontuacaoProfessor
+     * @throws java.lang.Exception
      */
     public PontuacaoProfessor() throws Exception{
         initComponents();
         var dao = new UsuarioDAO();
         String[] turmas = dao.getTurmas();
-        // jComboBox1. 
+        
+        for ( String turma: turmas){
+            jComboBox1.addItem(turma);
+        }
     }
 
     /**
@@ -31,25 +39,41 @@ public class PontuacaoProfessor extends javax.swing.JFrame {
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        tabelinhaDaAlergiaEEEEEEEEE = new javax.swing.JTable();
         jComboBox1 = new javax.swing.JComboBox<>();
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        tabelinhaDaAlergiaEEEEEEEEE.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Nome ", "Pontuação", "Data/Hora"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(tabelinhaDaAlergiaEEEEEEEEE);
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox1ActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel1.setText("Turmas:");
+
+        jButton1.setText("Pesquisar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -58,20 +82,57 @@ public class PontuacaoProfessor extends javax.swing.JFrame {
             .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton1)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(4, 4, 4)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1)
+                    .addComponent(jButton1))
+                .addGap(3, 3, 3)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 268, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jComboBox1ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // pegou o vaklor selecionado na combobox
+        String turma = jComboBox1.getSelectedItem().toString();
+
+        if (!"".equals(turma)){
+            try {
+                var dao = new UsuarioDAO();
+                String[][] dadosTabela = dao.buscarTurma(turma);               
+                for (int i = 0; i < dadosTabela.length; i++){
+                    for (int j = 0; j < 3; i++){
+                        tabelinhaDaAlergiaEEEEEEEEE.setValueAt(dadosTabela[i][j], i, j);
+                        
+                    }
+                }
+                
+                System.out.print(dadosTabela);
+            } catch (Exception ex) {
+                Logger.getLogger(PontuacaoProfessor.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            
+        }
+        // buscar/atualizar
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -103,14 +164,24 @@ public class PontuacaoProfessor extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
+<<<<<<< Updated upstream
                 //new PontuacaoProfessor().setVisible(true);
+=======
+                try {
+                    new PontuacaoProfessor().setVisible(true);
+                } catch (Exception ex) {
+                    Logger.getLogger(PontuacaoProfessor.class.getName()).log(Level.SEVERE, null, ex);
+                }
+>>>>>>> Stashed changes
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JTable tabelinhaDaAlergiaEEEEEEEEE;
     // End of variables declaration//GEN-END:variables
 }
